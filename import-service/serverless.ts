cc76221801bc390e9a71b8523bed472f3fa9048f
roboticstechnology <roboticstechnology@gmail.com>
2021-09-27 05:55:43 +0300

    feat: importfile lamda function

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,9 +1,10 @@
 import type { AWS } from '@serverless/typescript';
-
-import hello from '@functions/hello';
+// import { importProductsFile, importFileParser } from 'src/functions';
+import { importProductsFile } from 'src/functions';
 
 const serverlessConfiguration: AWS = {
-  service: 'import-service',
+  service: 'import-service-s3',
+  useDotenv: true,
   frameworkVersion: '2',
   custom: {
     webpack: {
@@ -15,17 +16,34 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: 'eu-west-1',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      S3_BUCKET: '${env:S3_BUCKET}'
     },
     lambdaHashingVersion: '20201221',
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: 's3:ListBucket',
+        Resource: 'arn:aws:s3:::${env:S3_BUCKET}'
+      },
+      {
+        Effect: 'Allow',
+        Action: 's3:*',
+        Resource: 'arn:aws:s3:::${env:S3_BUCKET}/*'
+      }
+    ],
   },
+
+
   // import the function via paths
-  functions: { hello },
+  //functions: { importProductsFile, importFileParser },
+  functions: { importProductsFile }
 };
 
 module.exports = serverlessConfiguration;
