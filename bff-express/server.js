04a773b7c2085c194864e54a34f8ac9a0c48a7d1
roboticstechnology <roboticstechnology@gmail.com>
2021-11-07 12:28:54 +0300

    featL add proxi functions for products/produc/cart endpoint

---
+++ bff-express/server.js
@@ -0,0 +1,60 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+
+dotenv.config();
+const log = console.log;
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.all('/*', async (req, res) => {
+    let status, message;
+    //https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev/products
+    //id:7e02a3ac-a564-4291-ab8b-9a36fb736246
+    // const { title, description, price, count } = event.body;
+    let urlPath = req.url.split('/')[1]
+    log(urlPath)
+    const url = process.env[urlPath]
+    if (url) {
+
+        try {
+            log(url);
+
+            let respResult = await axios({
+                method: req.method,
+                url: url + req.url,
+                ...(Object.keys(req.body || {}).length > 0 && { data: req.body }),
+            })
+            status = respResult.status
+            message = respResult.data
+            message.type = 'original'
+
+        } catch (e) {
+            log(e);
+            status = e.response.status;
+            message = e.message;
+
+        }
+
+        res.status(status).json(message)
+
+    } else {
+
+        res.status(502).send('Cannot process request')
+
+    }
+
+})
+
+app.listen(PORT, () => {
+    console.log(`app listening at http://localhost:${PORT}`)
+})
